2c6f8a814fae28f6d6efbf966acaf93b600df6bf
toktogazievb <toktogaziev21@gmail.com>
2024-07-23 19:00:43 +0600

    added second activity

--- app/src/main/java/com/example/mycalculatorapp/SecondActivity.java
+++ app/src/main/java/com/example/mycalculatorapp/SecondActivity.java
@@ -1,7 +1,9 @@
 package com.example.mycalculatorapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +15,8 @@ import com.google.android.material.slider.RangeSlider;
 
 public class SecondActivity extends AppCompatActivity {
 
+    private TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +27,18 @@ public class SecondActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-//       \;
+        Intent intentTwo = new Intent(SecondActivity.this, MainActivity.class);
+
+        textView = findViewById(R.id.result_text);
+        String text = getIntent().getStringExtra("key");
+        textView.setText(text);
     }
 
     public void onHeartClick(View view) {
-        view.setForeground();
+        view.setForeground(getDrawable(R.color.white));
     }
 
     public void onNextClick(View view) {
+        finish();
     }
 }
